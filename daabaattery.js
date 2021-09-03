5f65e2003300eb20d98b0c899f2788f888400b3d
Proking73 <77035338+Proking73@users.noreply.github.com>
2021-09-03 12:23:44 -0400

    Add files via upload

---
+++ daabaattery.js
@@ -0,0 +1,10 @@
+
+window.alert("This website is not complete and may have some bugs.");// how to refreash website??
+//why does it happen evry time i refresh? learn hw 2 make it only ekh time
+document.getElementsByClassName("js_buttons").innerHtml = ""; // change and put 
+console.log("test to see if running")
+var x = mouseX;
+var YEET;
+var YEET = shawn + "boi";
+var shawn;
+var shawn = "shawny"
