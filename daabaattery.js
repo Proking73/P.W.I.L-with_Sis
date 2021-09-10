a767bae40866a55e4e7075e319cc44ccba3fbe51
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-09-10 18:08:31 -0400

    rewatcvhed vid and svaed changes

--- daabaattery.js
+++ daabaattery.js
@@ -3,7 +3,7 @@ window.alert("This website is not complete and may have some bugs.");// how to r
 //why does it happen evry time i refresh? learn hw 2 make it only ekh time
 document.getElementsByClassName("js_buttons").innerHtml = ""; // change and put 
 console.log("test to see if running")
-var x = mouseX;
+//var x = mouseX;
 var YEET;
 var shawn;
 var shawn = "shawny"
