0b40c464dd996559b2b27348b02d94960845afc6
Proking73 <MY_NAME@example.com>
2021-10-01 19:59:45 -0400

    ugh

--- daabaattery.js
+++ daabaattery.js
@@ -13,11 +13,16 @@ var YEET = shawn + "boi";
 document.getElementById("js_textAdjust").innerHTML = YEET;
 document.getElementsById("WWaterLava").style.backgroundImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6vZ7cs-Ad85zHr6hJZDhmLx9shWs6nxy00A&usqp=CAU";
 
-   document.getElementById("dontedit").innerHTML = "You can edit the title...";
-function jusType() {
+   
+function Check() {
  if (document.getElementById("title").value = "Practicing everything I learn, restarting /w Aarushi® edition")
  {
-    document.querySelector("#dontedit").innerHTML = "DON'T EDIT ZE TITLE!!!";
- } 
+    document.getElementById("dontedit").innerHTML = "You can edit the title...";
+    
+ } else {
+   function jusType() {
+   document.querySelector("#dontedit").innerHTML = "DON'T EDIT ZE TITLE!!!";
+   }
+ }
 // dont semi colon on if statement conditions
 }
