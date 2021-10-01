ecbe238f4417d5d67baa63889fb53d0d080223b6
Proking73 <MY_NAME@example.com>
2021-10-01 19:46:04 -0400

    scrollbar

--- daabaattery.js
+++ daabaattery.js
@@ -1,5 +1,5 @@
 
- window.alert("This website is not complete and may have some bugs.");// how to refreash website??
+// window.alert("This website is not complete and may have some bugs.");// how to refreash website??
 
 //why does it happen evry time i refresh? learn hw 2 make it only ekh time
 document.getElementsByClassName("js_buttons").innerHtml = ""; // change and put 
@@ -12,3 +12,12 @@ var YEET = shawn + "boi";
 
 document.getElementById("js_textAdjust").innerHTML = YEET;
 document.getElementsById("WWaterLava").style.backgroundImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6vZ7cs-Ad85zHr6hJZDhmLx9shWs6nxy00A&usqp=CAU";
+function jusType() {
+ if (document.getElementById("title").innerHTML = "Practicing everything I learn, restarting /w Aarushi® edition")
+ {
+    document.querySelector("#dontedit").innerHTML = "lel";
+ } else {
+     
+ }
+// dont semi colon on if statement conditions
+}
