4539876a682aaa19ffde7dc11b4b54d0d754e4bc
Proking73 <MY_NAME@example.com>
2021-10-02 17:17:21 -0400

    idk

--- daabaattery.js
+++ daabaattery.js
@@ -1,5 +1,5 @@
 
-// window.alert("This website is not complete and may have some bugs.");// how to refreash website??
+ window.alert("This website is not complete and may have some bugs.");// how to refreash website??
 
 //why does it happen evry time i refresh? learn hw 2 make it only ekh time
 document.getElementsByClassName("js_buttons").innerHtml = ""; // change and put 
@@ -14,21 +14,10 @@ document.getElementById("js_textAdjust").innerHTML = YEET;
 document.getElementsById("WWaterLava").style.backgroundImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6vZ7cs-Ad85zHr6hJZDhmLx9shWs6nxy00A&usqp=CAU";
 
 function jusType() {
-<<<<<<< HEAD
   if (document.querySelector("#title").value = "Practicig everything I learn, restarting /w Aarushi® edition") {
     window.alert("you can edit");
   } else {
     window.alert("hurr");
   }
-=======
- if (document.getElementById("title").value != "Practicing everything I learn, restarting /w Aarushi® edition")
- {
-    document.querySelector("#dontedit").innerHTML = "DON'T EDIT ZE TITLE!!!";
- } else {
-    document.getElementById("dontedit").innerHTML = "You can edit the title...";
-
- }
-// dont semi colon on if statement conditions
->>>>>>> 85ab6a8d084f8e3bbbdfba6635c8eba13090c5ca
 }
 // dont semi colon on if statement conditions
