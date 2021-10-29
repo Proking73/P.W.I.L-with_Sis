0657d0dd4da46fcd7f00d106f2210c508f3dc049
77035338 <MY_NAME@example.com>
2021-10-29 17:32:51 -0400

    adlike

--- daabaattery.js
+++ daabaattery.js
@@ -3,21 +3,25 @@
 
 //why does it happen evry time i refresh? learn hw 2 make it only ekh time
 document.getElementsByClassName("js_buttons").innerHtml = ""; // change and put 
-console.log("test to see if running")
+console.log("test to see if running");
 //var x = mouseX;
-var YEET;
-var shawn;
-var shawn = "shawny"
+//-------------------------------------------------
+var shawn = "shawny";
 var YEET = shawn + "boi";
-
 document.getElementById("js_textAdjust").innerHTML = YEET;
-document.getElementsById("WWaterLava").style.backgroundImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6vZ7cs-Ad85zHr6hJZDhmLx9shWs6nxy00A&usqp=CAU";
+//------------------------------------------------
+
+document.querySelector("#jsClicktest").addEventListener("click", function(){
+  document.getElementById("adlike").style.display = "none";
+  document.getElementById("showcat").style.display = "block";
+});
+
+//setTimeout
 
 function jusType() {
-  if (document.querySelector("#title").value = "Practicig everything I learn, restarting /w Aarushi® edition") {
-    window.alert("you can edit");
-  } else {
-    window.alert("hurr");
-  }
-}
+  let titlename = document.getElementById("");
+};
+
+var keke = "jeff";
 // dont semi colon on if statement conditions
+//document.getElementsById("WWaterLava").style.backgroundImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6vZ7cs-Ad85zHr6hJZDhmLx9shWs6nxy00A&usqp=CAU";
